463407f390154ab0369cb92be81e0848893cc80a
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-08-20 16:27:05 +0200

    version_1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/t-shirt_designer_three.js',
+  base: '/t-shirt_designer_three.js/',
   plugins: [react()],
 })
